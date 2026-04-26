491cf63d8659ed3af8840f7c0adee1868b890dbd
Rafi547 <rafi.myp15@gmail.com>
2026-04-26 11:24:15 +0530

    Add and Sub

---
+++ input_running.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+Num1=$1
+Num2=$2
+Add=$(($Num1+$Num2))
+Sub=$(($Num1-$Num2))
+echo "*************Addition*************"
+echo "Addition of two numbers $Num1 and $Num2 is: $Add"
+echo "*************Substraction*********"
+echo "Substraction of two numbers $Num1 and $Num2 is: $Sub"
+
